0bc65aea48704877ae972c8b02c665e29749b70b
Quintavalle <147600523+Quintavalle@users.noreply.github.com>
2023-10-27 18:44:08 +0200

    somma di due double come interi

---
+++ Esercizio3/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double x = 2.0;
+        double y = 3.0;
+        int z = (int)x + (int)y;
+        System.out.println("somma: "+z);
+    }
+}
