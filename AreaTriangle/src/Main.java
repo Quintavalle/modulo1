2d2d9aea4d46b44d68ffd3f10594984f360c5f8b
Quintavalle <147600523+Quintavalle@users.noreply.github.com>
2023-10-27 18:00:42 +0200

    area del triangolo

---
+++ AreaTriangle/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float base = 4;
+        float height = 8,area ;
+        area = ( base*height) / 2 ;
+        System.out.println("Area of Triangle is: "+area);
+    }
+}
