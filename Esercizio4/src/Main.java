f24819cbc3ea1d87d6ab51d911896ea300b73d4f
Quintavalle <147600523+Quintavalle@users.noreply.github.com>
2023-10-27 19:51:30 +0200

    somma di due char che restituisce il valore ASCII

---
+++ Esercizio4/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        char char1 = 'a';
+        char char2 = 'b';
+        int sum = (int)char1 + (int)char2;
+
+        System.out.println("La somma dei valori ASCII di: " + char1 + " e " + char2 + " é: " + sum);
+    }
+}
