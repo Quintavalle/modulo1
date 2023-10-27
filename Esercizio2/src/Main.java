ee8f601776c4f53597d9d3c161861ad3e6dd547c
Quintavalle <147600523+Quintavalle@users.noreply.github.com>
2023-10-27 18:29:17 +0200

    somma di due caratteri come stringa

---
+++ Esercizio2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char a = 'a';
+        char b = 'b';
+        String sommaAb = "" +a +b;
+        System.out.println("sommaAb: "+sommaAb);
+    }
+}
